0b718cb200744a0ab5ddf62c14fbe8c3c9a4bf0d
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-02-20 12:48:58 -0600

    Mobile response, but no drop down on the hamburger btn

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavHeader from './components/NavHeader';
+import Services from './components/Services';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    
+        <NavHeader />
+     
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavHeader.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faBars from "@fortawesome/fontawesome-free-solid/faBars";
+
+export default function NavHeader(){
+    return(
+        <div className="welcome-container">
+            <header className="navbar">
+                <a className="logo">Start Bootstrap</a>
+                    <div className="ham-btn">
+                        <button>MENU
+                        <FontAwesomeIcon className="bars" icon={faBars} />
+                        </button>
+                    </div>
+            </header>
+
+            <header className="navbar-lrg">
+            <div className="logo">Start Bootstrap</div>
+              <div className="menu-list">
+                <div className="menu-item">
+                   <a>SERVICES</a>
+                </div>
+                <div className="menu-item">
+                    <a>PORTFOLIO</a>
+                </div>
+                <div className="menu-item">
+                <a>ABOUT</a>
+                </div>
+                <div className="menu-item">
+                <a>TEAM</a>
+                </div>
+                <div className="menu-item">
+                <a>CONTACT</a>
+                </div>
+            </div>
+        </header>
+          
+          <div className="intro">
+            <h2>Welcome To Our Studio!</h2>
+            <h1>IT'S NICE TO MEET YOU</h1>
+          <div className="welcome-btn">TELL ME MORE</div>
+        </div>
+    </div>
+  );
+}
